11c809d3fef1d36cb17fe674f5ef0790c1a0acbe
cartagenes <junior_pjl@hotmail.com>
2023-09-03 11:03:43 -0300

    versao1

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -12,21 +12,26 @@ public class ContaTerminal {
 
        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-       System.out.println("Digite seu nome: ");
+       System.out.print("Digite seu nome: ");
        String nome = scanner.next();
 
-       System.out.println("Digite o número da conta: ");
+       System.out.print("Digite o número da agencia: ");
+       String numeroAgencia = scanner.next();   
+
+       System.out.print("Digite o número da conta: ");
        int numeroConta = scanner.nextInt();
 
-       System.out.println("Digite o número da agencia: ");
-       String numeroAgencia = scanner.next();       
-
-       System.out.println("Digite o saldo: ");
+      
+       System.out.print("Digite o saldo: ");
        double saldo = scanner.nextDouble();
 
-       System.out.println("Olá" + nome + "obrigado por criar uma conta em nosso banco, ");
-       System.out.println("sua agência é " + numeroAgencia + ", conta " + numeroConta);
-       System.out.println("e seu saldo "+ saldo + "ja esta disponível para saque.");
+       System.out.println("Olá " + nome + " obrigado por criar uma conta em nosso banco, seus dados são: ");
+
+       
+       System.out.println("Agência: " + numeroAgencia) ;
+       System.out.println("Conta: " + numeroConta);
+       System.out.println("Saldo: " + saldo );
+       System.out.println("Já esta disponível para saque.");
 
 
     }
